31c3e4b164986fb73635640f8f953d707477a329
Thiagoc <thiagoc1415@gmail.com>
2025-03-03 11:53:49 -0300

    feat: update Allowed hosts

--- eld_tracker_backend/eld_tracker_backend/settings.py
+++ eld_tracker_backend/eld_tracker_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%$%wsui=^wxw*^(am$wtxc)m893pvh2hpmav3at_+=88#)db(o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
